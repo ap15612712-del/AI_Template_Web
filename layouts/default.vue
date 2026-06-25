15d729338e079f46c6a68bbc2e5202f1be0132ae
ap15612712-del <ap15612712@gmail.com>
2026-06-25 15:10:51 +0700

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,13 @@ watch(() => route.path, () => { sidebarOpen.value = false; });
 
       <nav class="sb-nav">
         <div class="sb-section">Main</div>
+        <NuxtLink to="/getting-started" class="sb-link" :class="{ active: isActive('/getting-started') }">
+          <svg class="sb-icon" viewBox="0 0 16 16" fill="currentColor">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+          </svg>
+          Getting Started
+        </NuxtLink>
         <NuxtLink to="/" class="sb-link" :class="{ active: route.path === '/' }">
           <svg class="sb-icon" viewBox="0 0 16 16" fill="currentColor">
             <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
@@ -72,6 +79,14 @@ watch(() => route.path, () => { sidebarOpen.value = false; });
             <line x1="8" y1="2" x2="8" y2="14"/><line x1="2" y1="8" x2="14" y2="8"/>
           </svg>
           Add Department
+        </NuxtLink>
+
+        <div class="sb-section">Templates</div>
+        <NuxtLink to="/empty" class="sb-link" :class="{ active: isActive('/empty') }">
+          <svg class="sb-icon" viewBox="0 0 16 16" fill="currentColor">
+            <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
+          </svg>
+          Empty Pages
         </NuxtLink>
 
         <div class="sb-section">API</div>
